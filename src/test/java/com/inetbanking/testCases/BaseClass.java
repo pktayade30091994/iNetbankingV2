b4dbe0a28eb9acbfbf1a8ec65e3060c3bb0213b1
Sagar <Sagar@DESKTOP-0DPRSEC>
2023-07-27 23:11:17 -0700

    First Commit

---
+++ src/test/java/com/inetbanking/testCases/BaseClass.java
@@ -0,0 +1,28 @@
+package com.inetbanking.testCases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseClass {
+	
+	public String baseURL="https://demo.guru99.com/v4/index.php";
+	public String username="mngr517659";
+	public String password="tEmyjun";
+	public static WebDriver driver;
+	
+	@BeforeClass
+	public void setup()
+	{
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//Drivers//chromedriver.exe");
+		driver=new ChromeDriver();
+	}
+	
+	@AfterClass
+	public void tearDown()
+	{
+		driver.quit();
+	}
+
+}
