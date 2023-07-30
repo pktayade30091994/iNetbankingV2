8f915a9df3b400030e384b33a13079b63ed7bf5b
Sagar <Sagar@DESKTOP-0DPRSEC>
2023-07-30 12:40:17 -0700

    first commit

--- src/test/java/com/inetbanking/testCases/BaseClass.java
+++ src/test/java/com/inetbanking/testCases/BaseClass.java
@@ -1,22 +1,49 @@
 package com.inetbanking.testCases;
 
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import com.inetbanking.utilities.ReadConfig;
 
 public class BaseClass {
 	
-	public String baseURL="https://demo.guru99.com/v4/index.php";
-	public String username="mngr517659";
-	public String password="tEmyjun";
+	ReadConfig readconfig=new ReadConfig();
+	
+	public String baseURL=readconfig.getApplicationUrl();
+	public String username=readconfig.getUsername();
+	public String password=readconfig.getPassword();
 	public static WebDriver driver;
 	
+	public static Logger logger;
+	
+	@Parameters("browser")
 	@BeforeClass
-	public void setup()
+	public void setup(String br)
 	{
-		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//Drivers//chromedriver.exe");
+		
+		logger =Logger.getLogger("ebanking");
+		PropertyConfigurator.configure("Log4j.properties");
+		if(br.equals("chrome")){
+		System.setProperty("webdriver.chrome.driver",readconfig.getChromepath());
 		driver=new ChromeDriver();
+		}
+		else if(br.equals("microsoftEdge")) {
+			System.setProperty("webdriver.msedge.driver",readconfig.getEdgepath());
+			driver=new EdgeDriver();
+		}
+		else if(br.equals("mfirefox")) {
+			System.setProperty("webdriver.gecko.driver",readconfig.getFirefoxpath());
+			driver=new FirefoxDriver();
+		}
+		driver.get(baseURL);
 	}
 	
 	@AfterClass
--- src/test/java/com/inetbanking/testCases/TC_LoginTest_001.java
+++ src/test/java/com/inetbanking/testCases/TC_LoginTest_001.java
@@ -10,19 +10,24 @@ public class TC_LoginTest_001 extends BaseClass {
 	@Test
 	public void loginTest()
 	{
-		driver.get(baseURL);
+		
+		logger.info("URL is opened");
 		LoginPage lp=new LoginPage(driver);
 		lp.setUserName(username);
+		logger.info("Entered User Name");
 		lp.setPassword(password);
+		logger.info("Entered Password");
 		lp.clickSubmit();
 		
 		if(driver.getTitle().equals("Guru99 Bank Manager HomePage"))
 		{
 			Assert.assertTrue(true);
+			logger.info("Login Test pass");
 		}
 		else
 		{
 			Assert.assertTrue(false);
+			logger.info("LOgin Test fail");
 		}
 			
 		
